cea638f3eff62593ce1618fbc8f07db06c0c5673
Rinkesh Sante <rinkeshsante@gmail.com>
2025-04-15 13:42:38 +0530

    first

---
+++ src/test/java/com/management/project_management_be/ProjectManagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.project_management_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
